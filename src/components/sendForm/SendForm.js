163905a0c270a3429eb7b322733a50722f1b22bc
Alexander_Pashuk <pashuk.sasha18@gmail.com>
2021-12-05 18:12:28 +0200

    finish commit

--- src/components/sendForm/SendForm.js
+++ src/components/sendForm/SendForm.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import axios from "axios";
 import "./sendForm.scss"
 
 const SendForm = ({formState}) => {
-   
-    
-const handleSubmit = (event) => {
+  
+  const [date, setDate] = useState('')
+  const[temperatureC, setTemperatureC] = useState()
+  const[temperatureF, setTemperatureF] = useState()
+  const[summary, setSummary] = useState('')
 
-        event.preventDefault();
-        axios.post('https://localhost:44331/weatherforecast/post')
-          .then(res => {
-            console.log(res);
-            console.log(res.data);
-          })
+    const handleSubmit = () => {
+        axios.get('https://localhost:44331/weatherforecast/post/')
+            console.log('{"res":”ok”}')
+          } 
+
+
+    const handlDChange = (event) =>{
+        setDate(event.target.value);
+    }
+    const handleTCChange = (event) =>{
+      setTemperatureC(event.target.value);
+    }
+    const handleTFChange = (event) =>{
+    setTemperatureF(event.target.value);
+    }
+    const handleSChange = (event) =>{
+    setSummary(event.target.value);
     }
 
-    // const handleChange = (event) => {
-    //     console.log(event.target.value)
-    //     setData(event.target.value);
-    // }
 
     return(
        <>
@@ -29,12 +38,28 @@ const handleSubmit = (event) => {
               <span>TemperatureF:</span>
               <span>Summary:</span>
             </div>
-             <form  className={'sendForm__list'} onClick={handleSubmit} >
-              <input type='text' placeholder={formState ? formState.date.slice(0, 10) : ''}/>
-              <input type='text' placeholder={formState ? formState.temperatureC : ''}/>
-              <input type='text' placeholder={formState ? formState.temperatureF : ''}/>
-              <input type='text' placeholder={formState ? formState.summary : ''}/>
-             <input type='submit'/>
+             <form  className={'sendForm__list'} >
+              <input 
+              type='text' 
+              value={date}
+              onChange={handlDChange}
+              placeholder={formState ? formState.date.slice(0, 10) : ''}/>
+              <input 
+              type='text' 
+              value={temperatureC}
+              onChange={handleTCChange}
+              placeholder={formState ? formState.temperatureC : ''}/>
+              <input 
+              type='text' 
+              value={temperatureF}
+              onChange={handleTFChange}
+              placeholder={formState ? formState.temperatureF : ''}/>
+              <input 
+              type='text'
+              value={summary} 
+              onChange={handleSChange}
+              placeholder={formState ? formState.summary : ''}/>
+            <button onSubmit={handleSubmit}>send data</button>
              </form>
            </div>
        </>
